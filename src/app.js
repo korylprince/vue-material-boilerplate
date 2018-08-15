1d09ca86f3a388361bc3c64b74dc380840e5cbb7
Kory Prince <korylprince@gmail.com>
2018-08-15 17:59:23 -0500

    fix syntax change (eslint)

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ import router from "./js/router.js"
 import store from "./js/store.js"
 
 // if signed out, go to signin page
-store.watch((state, getters) => { return getters.signed_in }, signed_in => {
+store.watch((state, getters) => getters.signed_in, signed_in => {
     if (!signed_in) {
         router.push({name: "signin"})
     }
