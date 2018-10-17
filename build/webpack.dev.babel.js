7c1c0021b2e5d82322b742e788f2fcb277a4c836
Kory Prince <korylprince@gmail.com>
2018-10-17 11:17:11 -0500

    added hot module replacement

--- build/webpack.dev.babel.js
+++ build/webpack.dev.babel.js
@@ -1,3 +1,4 @@
+import webpack from "webpack"
 import merge from "webpack-merge"
 
 import baseConfig from "./webpack.base.babel.js"
@@ -6,12 +7,16 @@ const devConfig = {
     mode: "development",
     stats: {children: false},
     devServer: {
+        hot: true,
         stats: "minimal",
         proxy: {
             "/api": process.env.API_SERVER,
         },
     },
     devtool: "#cheap-module-inline-source-map",
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ],
 }
 
 export default merge(baseConfig, devConfig)
