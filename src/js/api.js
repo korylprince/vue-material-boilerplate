a0a811d53fb0397d2afacca7ad6ad5a4dbb5c595
Kory Prince <korylprince@gmail.com>
2019-10-21 13:58:17 -0500

    use template strings in api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,4 +1,4 @@
-/* global API_BASE*/
+/* global API_BASE */
 
 import store from "./store.js"
 
@@ -55,7 +55,7 @@ const api = {
         return promise
     },
     do_authenticated_thing(data) {
-        return store.getters.$http().post(API_BASE + "/data", data)
+        return store.getters.$http().post(`${API_BASE}/data`, data)
     },
 }
 
